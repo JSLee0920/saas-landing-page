79deb97df57e1375d381596f1c77635217ea3342
Lee Jia Sheng <leejiasheng0826@gmail.com>
2025-11-07 13:48:07 +0800

    feat: implement button component and navbar

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,28 @@
 import { HTMLAttributes } from "react";
+import { cva } from "class-variance-authority";
 
-export default function Button(props: HTMLAttributes<HTMLButtonElement>) {
-  return <button></button>;
+const classes = cva("border h-12 rounded-full px-6 font-medium", {
+  variants: {
+    variant: {
+      primary: "bg-lime-400 text-neutral-950 border-lime-500",
+      secondary: "border-white text-white bg-transparent",
+    },
+  },
+});
+
+export default function Button(
+  props: {
+    variant: "primary" | "secondary";
+  } & HTMLAttributes<HTMLButtonElement>
+) {
+  const { variant, className, ...OtherProps } = props;
+  return (
+    <button
+      className={classes({
+        variant,
+        className,
+      })}
+      {...OtherProps}
+    />
+  );
 }
--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import logo from "@/assets/images/logo.svg";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Button from "@/components/Button";
 
 const navLinks = [
   { label: "Home", href: "#" },
@@ -13,18 +14,25 @@ const navLinks = [
 
 export default function Navbar() {
   return (
-    <section className="py-4">
-      <div className="container mx-auto px-4">
-        <div className="grid grid-cols-2 border border-white/15 rounded-full p-2 px-4 items-center">
+    <section className="py-4 lg:py-8">
+      <div className="container mx-auto px-4 max-w-6xl">
+        <div className="grid grid-cols-2 lg:grid-cols-3 border border-white/15 rounded-full p-2 px-4 md:pr-2 items-center">
           <div>
-            <Image src={logo} alt="Layers" className="h-9 w-auto" />
+            <Image src={logo} alt="Layers" className="h-9 md:h-auto w-auto" />
           </div>
-          <div className="flex justify-end">
+          <div className="lg:flex justify-center items-center hidden">
+            <nav className="flex gap-6 font-semibold">
+              {navLinks.map((link) => (
+                <a href={link.href} key={link.label}>
+                  {link.label}
+                </a>
+              ))}
+            </nav>
+          </div>
+          <div className="flex justify-end gap-4">
             <FontAwesomeIcon icon={faBars} className="w-6 h-6 md:hidden" />
-            <button className="border border-white h-12 rounded-full px-6 font-medium">
-              Log In
-            </button>
-            <button>Sign Up</button>
+            <Button variant="secondary">Log In</Button>
+            <Button variant="primary">Sign Up</Button>
           </div>
         </div>
       </div>
