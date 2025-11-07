ba90777fac753df667f19d1ada39fe343ad569d4
Lee Jia Sheng <leejiasheng0826@gmail.com>
2025-11-07 13:47:37 +0800

    feat: implement button component and navbar

---
+++ src/components/Button.tsx
@@ -0,0 +1,5 @@
+import { HTMLAttributes } from "react";
+
+export default function Button(props: HTMLAttributes<HTMLButtonElement>) {
+  return <button></button>;
+}
--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -13,14 +13,18 @@ const navLinks = [
 
 export default function Navbar() {
   return (
-    <section>
-      <div className="container">
-        <div className="grid grid-cols-2">
+    <section className="py-4">
+      <div className="container mx-auto px-4">
+        <div className="grid grid-cols-2 border border-white/15 rounded-full p-2 px-4 items-center">
           <div>
-            <Image src={logo} alt="Layers" />
+            <Image src={logo} alt="Layers" className="h-9 w-auto" />
           </div>
-          <div>
-            <FontAwesomeIcon icon={faBars} className="w-6 h-6" />
+          <div className="flex justify-end">
+            <FontAwesomeIcon icon={faBars} className="w-6 h-6 md:hidden" />
+            <button className="border border-white h-12 rounded-full px-6 font-medium">
+              Log In
+            </button>
+            <button>Sign Up</button>
           </div>
         </div>
       </div>
