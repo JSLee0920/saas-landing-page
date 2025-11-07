1ac0080c0047ccd1305a05ece0269062ab400237
Lee Jia Sheng <leejiasheng0826@gmail.com>
2025-11-07 09:31:22 +0800

    chore: add navbar component

---
+++ src/sections/Navbar.tsx
@@ -0,0 +1,11 @@
+const navLinks = [
+  { label: "Home", href: "#" },
+  { label: "Features", href: "#features" },
+  { label: "Integrations", href: "#integrations" },
+  { label: "Pricing", href: "#pricing" },
+  { label: "FAQs", href: "#faqs" },
+];
+
+export default function Navbar() {
+  return <div>Navbar</div>;
+}
