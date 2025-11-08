1f0bd15ad975274367c3344ac22a9721b5ff314b
Lee Jia Sheng <leejiasheng0826@gmail.com>
2025-11-08 11:46:16 +0800

    feat: implement cards

--- src/components/Key.tsx
+++ src/components/Key.tsx
@@ -6,7 +6,7 @@ export default function Key(props: HTMLAttributes<HTMLDivElement>) {
   return (
     <div
       className={twMerge(
-        "size-14 bg-neutral-300 inline-flex items-center justify-center",
+        "size-14 bg-neutral-300 inline-flex items-center justify-center rounded-2xl text-xl text-neutral-950 font-medium",
         className
       )}
       {...otherProps}
